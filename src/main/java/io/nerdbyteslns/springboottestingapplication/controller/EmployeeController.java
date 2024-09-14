5633d5af0bc9e78cab2f8d3c272a7bcade5ec074
Matthew Oduamafu <mattoduamafu@gmail.com>
2024-09-14 11:12:13 +0000

    add controller unit test and integration test

--- src/main/java/io/nerdbyteslns/springboottestingapplication/controller/EmployeeController.java
+++ src/main/java/io/nerdbyteslns/springboottestingapplication/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
@@ -22,5 +24,27 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.CREATED)
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        employee.setId(id);
+        return employeeService.updateEmployee(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
     }
 }
--- src/test/java/io/nerdbyteslns/springboottestingapplication/integration/EmployeeControllerIntegrationTests.java
+++ src/test/java/io/nerdbyteslns/springboottestingapplication/integration/EmployeeControllerIntegrationTests.java
@@ -1,17 +1,15 @@
-package io.nerdbyteslns.springboottestingapplication.controller;
+package io.nerdbyteslns.springboottestingapplication.integration;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.nerdbyteslns.springboottestingapplication.model.Employee;
-import io.nerdbyteslns.springboottestingapplication.service.EmployeeService;
+import io.nerdbyteslns.springboottestingapplication.repository.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,15 +17,15 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
-import java.util.Optional;
 
-@WebMvcTest(EmployeeController.class)
-public class EmployeeControllerTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerIntegrationTests {
 
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
-    private EmployeeService employeeService;
+    @Autowired
+    private EmployeeRepository employeeRepository;
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -35,6 +33,8 @@ public class EmployeeControllerTests {
 
     @BeforeEach
     void setUp() {
+        employeeRepository.deleteAll();
+
         employees = List.of(
                 Employee.builder()
                         .id(1L)
@@ -47,28 +47,9 @@ public class EmployeeControllerTests {
                         .firstName("Jane")
                         .lastName("Doe")
                         .email("jane.doe@example.com")
-                        .build(),
-                Employee.builder()
-                        .id(3L)
-                        .firstName("Bob")
-                        .lastName("Smith")
-                        .email("bob.smith@example.com")
-                        .build(),
-                Employee.builder()
-                        .id(4L)
-                        .firstName("Tom")
-                        .lastName("Jerry")
-                        .email("tom.jerry@example.com")
-                        .build(),
-                Employee.builder()
-                        .id(5L)
-                        .firstName("Alice")
-                        .lastName("Wonder")
-                        .email("alice.wonder@example.com")
                         .build()
         );
     }
-
 
     @DisplayName("Junit 5 Test to create a new Employee")
     @Test
@@ -79,8 +60,6 @@ public class EmployeeControllerTests {
                 .lastName("Doe")
                 .email("john.doe@example.com")
                 .build();
-        BDDMockito.given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
-                .willAnswer((invocation) -> invocation.getArgument(0));
 
         // when - action or the behaviour that we are going test
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post("/api/employees")
@@ -90,28 +69,17 @@ public class EmployeeControllerTests {
         // then - verify the output
         response.andExpect(result -> {
             Employee savedEmployee = objectMapper.readValue(result.getResponse().getContentAsString(), Employee.class);
-//            assert savedEmployee.getId() != 0;
             assert savedEmployee.getFirstName().equals(employee.getFirstName());
             assert savedEmployee.getLastName().equals(employee.getLastName());
             assert savedEmployee.getEmail().equals(employee.getEmail());
         }).andDo(MockMvcResultHandlers.print());
-//        response.andExpect(MockMvcResultMatchers.status().isCreated())
-//                .andExpect(MockMvcResultMatchers.jsonPath("id").exists())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())))
-//                .andDo(MockMvcResultHandlers.print());
     }
-
 
     @DisplayName("Junit 5 Test to get all Employees")
     @Test
     public void givenEmployees_whenGetAllEmployees_thenReturnAllEmployees() throws Exception {
         // given - precondition or setup
-
-
-        BDDMockito.given(employeeService.getAllEmployees())
-                .willReturn(employees);
+        employeeRepository.saveAll(employees);
 
         // when - action or the behaviour that we are going test
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/employees"));
@@ -127,31 +95,28 @@ public class EmployeeControllerTests {
         response.andExpect(MockMvcResultMatchers.status().isOk());
     }
 
+
     @DisplayName("Junit 5 Test to get Employee by Id")
     @Test
     public void givenEmployeeId_whenGetEmployeeById_thenReturnEmployee() throws Exception {
         // given - precondition or setup
         long employeeId = 1L;
         Employee emp = Employee.builder()
-                .id(employeeId)
                 .firstName("John")
                 .lastName("Doe")
-                .email("johndoe#example.com")
+                .email("johndoe@example.com")
                 .build();
-
-        BDDMockito.given(employeeService.getEmployeeById(employeeId))
-                .willReturn(java.util.Optional.of(emp));
+        employeeId =  (employeeRepository.save(emp)).getId();
 
         // when - action or the behaviour that we are going test
-        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/{id}", employeeId));
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/{id}" , employeeId));
 
         // then - verify the output
         response.andExpect(result -> {
             Employee employee = objectMapper.readValue(result.getResponse().getContentAsString(), Employee.class);
-            assert employee.getId() == 1L;
             assert employee.getFirstName().equals("John");
             assert employee.getLastName().equals("Doe");
-            assert employee.getEmail().equals("johndoe#example.com");
+            assert employee.getEmail().equals("johndoe@example.com");
         }).andDo(MockMvcResultHandlers.print());
         response.andExpect(MockMvcResultMatchers.status().isOk());
     }
@@ -162,16 +127,14 @@ public class EmployeeControllerTests {
         // given - precondition or setup
         long employeeId = 1L;
 
-        BDDMockito.given(employeeService.getEmployeeById(employeeId))
-                .willReturn(Optional.empty());
-
         // when - action or the behaviour that we are going test
-        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/{id}", employeeId));
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/{id}" , employeeId));
 
         // then - verify the output
         response.andExpect(MockMvcResultMatchers.status().isOk());
         response.andExpect(MockMvcResultMatchers.content().string(""));
     }
+
 
     @DisplayName("Junit 5 Test to update Employee by Id")
     @Test
@@ -184,13 +147,11 @@ public class EmployeeControllerTests {
                 .lastName("Doe")
                 .email("johndoe@gmail.com")
                 .build();
-        BDDMockito.given(employeeService.getEmployeeById(employeeId))
-                .willReturn(java.util.Optional.of(emp));
+        employeeId =  (employeeRepository.save(emp)).getId();
         emp.setFirstName("Jane");
         emp.setLastName("Doe");
         emp.setEmail("janedoe@gmail.com");
-        BDDMockito.given(employeeService.updateEmployee(ArgumentMatchers.any(Employee.class)))
-                .willAnswer((invocation) -> invocation.getArgument(0));
+        employeeRepository.save(emp);
 
         // when - action or the behaviour that we are going test
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders.put("/api/employees/{id}", employeeId)
@@ -200,7 +161,6 @@ public class EmployeeControllerTests {
         // then - verify the output
         response.andExpect(result -> {
             Employee updatedEmployee = objectMapper.readValue(result.getResponse().getContentAsString(), Employee.class);
-            assert updatedEmployee.getId() == 1L;
             assert updatedEmployee.getFirstName().equals("Jane");
             assert updatedEmployee.getLastName().equals("Doe");
             assert updatedEmployee.getEmail().equals("janedoe@gmail.com");
@@ -218,8 +178,7 @@ public class EmployeeControllerTests {
                 .lastName("Doe")
                 .email("johndoe@gmail.com")
                 .build();
-        BDDMockito.given(employeeService.getEmployeeById(employeeId))
-                .willReturn(java.util.Optional.of(emp));
+        employeeId =  (employeeRepository.save(emp)).getId();
 
         // when - action or the behaviour that we are going test
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders.delete("/api/employees/{id}", employeeId));
